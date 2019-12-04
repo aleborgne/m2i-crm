5eaca4f75886cda5a07d1b73de6516a2bf2c46d5
Alexandre <alex.e.leb@gmail.com>
2019-12-04 17:05:17 +0100

    update changeDetectionStrategy, use async pipe for observables, fix async bug

--- src/app/prestations/pages/page-list-prestations/page-list-prestations.component.ts
+++ src/app/prestations/pages/page-list-prestations/page-list-prestations.component.ts
@@ -1,6 +1,7 @@
-import { State } from './../../../shared/enums/state.enum';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { Prestation } from 'src/app/shared/models/prestation';
+import { State } from './../../../shared/enums/state.enum';
 import { PrestationsService } from './../../services/prestations.service';
 
 @Component({
@@ -9,23 +10,22 @@ import { PrestationsService } from './../../services/prestations.service';
   styleUrls: ['./page-list-prestations.component.scss']
 })
 export class PageListPrestationsComponent implements OnInit {
-  public collection: Prestation[];
+  public collection$: Observable<Prestation[]>;
   public states = State;
   public headers = ['Type', 'Client', 'Nb Jours', 'TJM HT', 'Total HT', 'Total TTC', 'State'];
 
-  constructor(private prestationService: PrestationsService) { }
+  constructor(private prestationService: PrestationsService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.prestationService.collection.subscribe((data) => {
-      console.log(data);
-      this.collection = data;
-    });
+    this.collection$ = this.prestationService.collection;
   }
 
   changeState(item: Prestation, event) {
     this.prestationService.update(item, event.target.value).subscribe((res: Prestation) => {
       // item = res; doesn't emit ngOnChanges event
       item.state = res.state;
+      this.cdr.markForCheck(); // Redemander un Change detection pour éviter un bug de décalage asynchrone.
+      // Le bug en question: https://netbasal.com/a-comprehensive-guide-to-angular-onpush-change-detection-strategy-5bac493074a4
     });
   }
 }
